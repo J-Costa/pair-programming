e47df008552f0d723d1e53eb29727b6e3afb97b7
Josue Costa <juncosta@gmail.com>
2021-04-12 19:08:42 -0300

    add testes horinzontal e vertical

--- lib/word_hunter.rb
+++ lib/word_hunter.rb
@@ -1,2 +1,24 @@
 class WordHunter
-end
+    def self.generate_word(matrix, words)
+      # vowels = %w[a e i o u]
+      found_words = ''
+      matrix.each do |word| 
+        if word.join.include?(words)
+          found_words += words
+        end
+      end
+      
+      matrix.each_with_index do |word, index|
+        w = ''
+        matrix.each do |letters|
+          w += letters[index]
+          # found_words += letters[index]
+        end
+        if w.include?(words)
+          found_words += w
+        end
+      end
+      found_words
+    end
+  end
+  
--- spec/word_hunter_spec.rb
+++ spec/word_hunter_spec.rb
@@ -6,5 +6,20 @@ describe 'WordHunter' do
               ['J', 'U', 'S'],
               ['N', 'G', 'S']]
     words = 'BUG'
+
+    result = WordHunter.generate_word(matrix, words)
+
+    expect(result).to include(words)
+  end
+
+  it 'Recebe uma matriz e lista, e retorna palavra na horizontal' do
+    matrix = [['B', 'S', 'Z'],
+              ['U', 'O', 'S'],
+              ['G', 'L', 'S']]
+    words = 'BUG'
+    
+    result = WordHunter.generate_word(matrix, words)
+
+    expect(result).to include(words)
   end
 end
